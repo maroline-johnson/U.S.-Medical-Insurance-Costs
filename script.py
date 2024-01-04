04b30a593cf73b46ed2db1f9236ad50f63904839
Maroline Johnson <151571230+maroline-johnson@users.noreply.github.com>
2024-01-04 15:04:53 -0600

    Calculate average age of patients

--- script.py
+++ script.py
@@ -1,5 +1,15 @@
 import csv
 
+# Get patients info from insurance.csv
+patients = []
 with open('insurance.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    print(reader)
+    csv_reader = csv.DictReader(csvfile)
+    for row in csv_reader:
+        patients.append(row)
+
+# Calculate average age of patients
+total_age = 0
+for patient in patients:
+    total_age += int(patient['age'])
+average_age = round(total_age/len(patients))
+print(average_age)  # Prints 39
