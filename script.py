7bbd161ee3cd6143a15078cc57dd67ad73b1bb66
Maroline Johnson <151571230+maroline-johnson@users.noreply.github.com>
2024-01-04 15:22:59 -0600

    Analyze where a majority of patients are from

--- script.py
+++ script.py
@@ -12,4 +12,16 @@ total_age = 0
 for patient in patients:
     total_age += int(patient['age'])
 average_age = round(total_age/len(patients))
-print(average_age)  # Prints 39
+# print(average_age)  # Prints 39
+
+# Analyze where a majority of patients are from
+count = 0
+count_patients_per_region = {}
+for patient in patients:
+    region = patient['region']
+    if region not in count_patients_per_region:
+        count_patients_per_region[region] = count
+    for region in count_patients_per_region:
+        count_patients_per_region[region] += 1
+max_count_patients_per_region = max(count_patients_per_region) # southwest has the max
+# print("The " + str(max_count_patients_per_region.title()) + " region contains the highest number of patients in this dataset.")
