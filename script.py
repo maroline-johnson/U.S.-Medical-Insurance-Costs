6306856bc83418375b54e30730f3d4e469d7449a
Maroline Johnson <151571230+maroline-johnson@users.noreply.github.com>
2024-01-05 13:29:24 -0600

    Create function to calculate average age

--- script.py
+++ script.py
@@ -7,12 +7,18 @@ with open('insurance.csv') as csvfile:
     for row in csv_reader:
         patients.append(row)
 
+
+def calculate_average_age(total_num, csv_column):
+    for patient in patients:
+        total_num += int(patient[csv_column])
+    average = round(total_num / len(patients))
+    return average
+
+
 # Calculate average age of patients
 total_age = 0
-for patient in patients:
-    total_age += int(patient['age'])
-average_age = round(total_age/len(patients))
-# print("Average age of patients in this dataset: " + str(average_age))
+average_age = calculate_average_age(total_age, 'age')
+print("Average age of patients in this dataset: " + str(average_age))
 
 # Analyze where a majority of patients are from
 count = 0
@@ -23,7 +29,7 @@ for patient in patients:
         count_patients_per_region[region] = count
     for region in count_patients_per_region:
         count_patients_per_region[region] += 1
-max_count_patients_per_region = max(count_patients_per_region) # southwest has the max
+max_count_patients_per_region = max(count_patients_per_region)  # southwest has the max
 # print("Region with highest number of patients in this dataset: " + str(max_count_patients_per_region.title()))
 
 # Analyze average insurance cost for smokers vs. non-smokers and how it affects average cost overall
@@ -33,7 +39,7 @@ num_patients = 0
 for patient in patients:
     total_cost += float(patient['charges'])
     num_patients += 1
-average_cost = round(total_cost/num_patients)
+average_cost = round(total_cost / num_patients)
 # Get average cost for smokers
 total_cost_smokers = 0
 num_smokers = 0
@@ -41,7 +47,7 @@ for patient in patients:
     if patient['smoker'] == "yes":
         total_cost_smokers += float(patient['charges'])
         num_smokers += 1
-average_cost_smokers = round(total_cost_smokers/num_smokers)
+average_cost_smokers = round(total_cost_smokers / num_smokers)
 # Get average cost for non-smokers
 total_cost_nonsmokers = 0
 num_nonsmokers = 0
@@ -49,7 +55,7 @@ for patient in patients:
     if patient['smoker'] == "no":
         total_cost_nonsmokers += float(patient['charges'])
         num_nonsmokers += 1
-average_cost_nonsmokers = round(total_cost_nonsmokers/num_nonsmokers)
+average_cost_nonsmokers = round(total_cost_nonsmokers / num_nonsmokers)
 # Get factor of average insurance cost for smoker to non-smoker
 factor = round(average_cost_smokers / average_cost_nonsmokers)
 # print("Average insurance cost: $" + str(average_cost))
