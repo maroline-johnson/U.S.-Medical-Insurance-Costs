69a771efc9ff20210da4a1fd92fff9a4cf01a69e
Maroline Johnson <151571230+maroline-johnson@users.noreply.github.com>
2024-01-08 11:59:30 -0600

    Analyze what percent of each sex group are smokers/non-smokers

--- script.py
+++ script.py
@@ -43,7 +43,7 @@ def get_total_if(csv_if_column, operator, value, sum_value, type):
     return round(total, 2)
 
 
-# Get counts
+# Get common counts
 count_females = get_count('sex', 'female')
 count_males = get_count('sex', 'male')
 count_patients = count_females + count_males
@@ -60,10 +60,9 @@ total_bmi_males = get_total_if('sex', 'equal to', 'male', 'bmi', float)
 total_cost_females = get_total_if('sex', 'equal to', 'female', 'charges', float)
 total_cost_males = get_total_if('sex', 'equal to', 'male', 'charges', float)
 
-
 # Calculate average age of patients
 average_age = round(total_age / count_patients)
-# print("Average age of patients in this dataset: " + str(average_age))
+print("Average age of patients in this dataset: " + str(average_age))
 
 # Analyze where a majority of patients are from
 count = 0
@@ -75,7 +74,7 @@ for patient in patients:
     for region in count_patients_per_region:
         count_patients_per_region[region] += 1
 max_count_patients_per_region = max(count_patients_per_region)  # southwest has the max
-# print("Region with the highest number of patients in this dataset: " + str(max_count_patients_per_region.title()))
+print("Region with the highest number of patients in this dataset: " + str(max_count_patients_per_region.title()))
 
 # Analyze average insurance cost for smokers vs. non-smokers and how it affects average cost overall
 average_cost = round(total_cost / count_patients)
@@ -83,11 +82,11 @@ average_cost_smokers = round(total_cost_smokers / count_smokers)
 average_cost_nonsmokers = round(total_cost_non_smokers / count_non_smokers)
 # Get factor of average insurance cost for smoker to non-smoker
 factor = round(average_cost_smokers / average_cost_nonsmokers)
-# print("Average insurance cost: $" + str(average_cost))
-# print("Average insurance cost for smokers: $" + str(average_cost_smokers))
-# print("Average insurance cost for non-smokers: $" + str(average_cost_nonsmokers))
-# print("Smokers' average insurance cost is about " + str(factor) +
-# "x more than average insurance cost for non-smokers.")
+print("Average insurance cost: $" + str(average_cost))
+print("Average insurance cost for smokers: $" + str(average_cost_smokers))
+print("Average insurance cost for non-smokers: $" + str(average_cost_nonsmokers))
+print("Smokers' average insurance cost is about " + str(factor) +
+      "x more than average insurance cost for non-smokers.")
 
 # Analyze average age of patients with at least one child
 total_age_patients_with_child = 0
@@ -97,16 +96,29 @@ for patient in patients:
         total_age_patients_with_child += int(patient['age'])
         num_patients_with_child += 1
 average_age_patients_with_child = round(total_age_patients_with_child / num_patients_with_child)
-# print("Average age of patients with one or more children:", average_age_patients_with_child)
+print("Average age of patients with one or more children:", average_age_patients_with_child)
 
 # Analyze average BMI per sex
 average_bmi_females = round(total_bmi_females / count_females)
 average_bmi_males = round(total_bmi_males / count_males)
-# print("Average BMI for females:", average_bmi_females)
-# print("Average BMI for males:", average_bmi_males)
+print("Average BMI for females:", average_bmi_females)
+print("Average BMI for males:", average_bmi_males)
 
 # Analyze insurance cost per sex
 average_cost_females = round(total_cost_females / count_females)
 average_cost_males = round(total_cost_males / count_males)
 print("Average cost for females:", average_cost_females)
 print("Average cost for males:", average_cost_males)
+
+# Analyze what percent of each sex group are smokers/non-smokers
+count_female_smokers = 0
+count_male_smokers = 0
+for patient in patients:
+    if patient['sex'] == 'female' and patient['smoker'] == 'yes':
+        count_female_smokers += 1
+    elif patient['sex'] == 'male' and patient['smoker'] == 'yes':
+        count_male_smokers += 1
+percent_female_smokers = round((count_female_smokers / count_females * 100))
+percent_male_smokers = round((count_male_smokers / count_males * 100))
+print("Percent of females that smoke: " + str(percent_female_smokers) + "%")
+print("Percent of males that smoke: " + str(percent_male_smokers) + "%")
